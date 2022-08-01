7cf0f71ead6aec1ebb3d2d2d35da05dd681902ea
KelvinSilvaDev <63024154+KelvinSilvaDev@users.noreply.github.com>
2022-08-01 17:44:42 -0300

    fix: jsx

--- components/Dropbtn.jsx
+++ components/Dropbtn.jsx
@@ -4,9 +4,9 @@ import * as Style from "../styles/DropbtnStyles";
 export default function Dropbtn({ title, children }) {
   return (
     <Style.All>
-      <div class="dropdown">
-        <button class="dropbtn">{title}</button>
-        <div class="dropdown-content">{children}</div>
+      <div className="dropdown">
+        <button className="dropbtn">{title}</button>
+        <div className="dropdown-content">{children}</div>
       </div>
     </Style.All>
   );
